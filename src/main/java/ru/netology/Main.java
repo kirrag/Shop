8eeb57e23091f0db9e966f436bcc2c970391286b
Garrik Ten <ten@otux.ru>
2023-04-13 21:52:53 +0300

    sync

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,5 +1,10 @@
 package ru.netology;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
 	
 	// Magic rule
@@ -7,9 +12,31 @@ public class Main {
 	private static Product hleb = new Product("Хлеб", 50.0);
 	private static Product grechka = new Product("Гречневая крупа", 150.0);
 
+	private static Scanner scanner = new Scanner(System.in);
+
+	private static void printMenu(List<Product> products) {
+		for (Product product : products) {
+			System.out.println(product.toString());
+		}
+		System.out.println("Для завершения покупки введите \"end\"");
+	}
+
 	public static void main(String[] args) {
 
+		List<Product> products = new ArrayList<>();
+		products.add(moloko);
+		products.add(hleb);
+		products.add(grechka);
+
 		Basket basket = new Basket();
+
+		printMenu(products);
+
+
+		while (true) {
+			String input = scanner.nextLine().trim();
+			if ("end".equals(input)) break;
+		}
 
 		basket.add(moloko, 2);
 		basket.add(hleb, 1);
